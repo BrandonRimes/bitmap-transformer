4aefadc528902725609809788b049fffe637873e
Haustin Kimbrough <haustin.kimbrough@hotmail.com>
2021-09-23 22:21:27 -0700

    adds one transformation, needs work

--- app/src/main/java/bitmap/transformer/App.java
+++ app/src/main/java/bitmap/transformer/App.java
@@ -3,14 +3,19 @@
  */
 package bitmap.transformer;
 
+import java.io.IOException;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
-        System.out.println(new Bitmap(args); // new Bitmap().transform();
-//        System.out.println(args[0]);
-//        System.out.println(args[1]);
+    public static void main(String[] args) throws IOException {
+        // new Bitmap().transform();
+        System.out.println(new Bitmap().getImage("./app/src/main/resources/baldy-8bit.bmp"));
+/*
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+*/
     }
 }
