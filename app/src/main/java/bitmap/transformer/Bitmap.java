a84fbf8c02d353decc4e4fc1524808322cbc6b1f
Brandon Rimes <77939170+BrandonRimes@users.noreply.github.com>
2021-09-23 21:11:46 -0400

    bitmap component created. gradle initialized

---
+++ app/src/main/java/bitmap/transformer/Bitmap.java
@@ -0,0 +1,13 @@
+package bitmap.transformer;
+
+import java.awt.image.BufferedImage;
+
+public class Bitmap {
+    public String getGreeting2() {
+        return "Goodbye (cruel) World!";
+    }
+    BufferedImage img;
+    public transform1(img) {
+        ImageIO.write(img, "BMP", )
+    }
+}
